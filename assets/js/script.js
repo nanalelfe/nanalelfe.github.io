bcbe5e8462b7edaca6237a660292ee74741f1d89
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-08-30 22:17:17 -0400

    Minor changes, page scrollable

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function main() {
+    $('.scroll-link').on('click', function(event){
+        event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    scrollToSection(sectionID, 600);
+    });
+
+    $('#navbar-toggle').on('click', function (event) {
+        event.preventDefault();
+        $('#main-nav').toggleClass("open");
+    });
+}
+
+
+function scrollToSection(id, speed) {
+    var section = '#' + id;
+    var nav = $('#main-nav');
+    var offset = $(section).offset().top - 50;
+    $('html, body').animate({scrollTop: offset}, speed);
+
+    if (nav.hasClass("open")) {
+        nav.css("height", "1px").removeClass("in").addClass("collapse");
+        nav.removeClass("open");
+    }
+}
+$(document).ready(main);
