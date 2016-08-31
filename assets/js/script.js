7ca360a096fb8b423d1c4e9f77e2a19285e03c8d
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-08-31 00:21:52 -0400

    Fixed scolling and nav element highlighting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 function main() {
+
+    $('body').scrollspy({ target: '.navbar', offset: 70 });
+
     $('.scroll-link').on('click', function(event){
         event.preventDefault();
-    var sectionID = $(this).attr("data-id");
-    scrollToSection(sectionID, 600);
+        var sectionID = $(this).attr("data-id");
+        scrollToSection(sectionID, 700);
     });
 
     $('#navbar-toggle').on('click', function (event) {
@@ -14,13 +17,13 @@ function main() {
 
 function scrollToSection(id, speed) {
     var section = '#' + id;
-    var nav = $('#main-nav');
+    var $nav = $('#main-nav');
     var offset = $(section).offset().top - 50;
     $('html, body').animate({scrollTop: offset}, speed);
 
-    if (nav.hasClass("open")) {
-        nav.css("height", "1px").removeClass("in").addClass("collapse");
-        nav.removeClass("open");
+    if ($nav.hasClass("open")) {
+        $nav.css("height", "1px").removeClass("in").addClass("collapse");
+        $nav.removeClass("open");
     }
 }
 $(document).ready(main);
