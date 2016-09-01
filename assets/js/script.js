7ff6e12e1af7f41645deb2623eee4325f1641c41
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-09-01 03:31:07 -0400

    Added animated progress bars and set up parallax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,50 @@
 function main() {
 
+    /* -----------------------------------------------------------------------------------
+     * Parallax effect
+     * -----------------------------------------------------------------------------------*/
+
+    var $documentElement = $(document);
+    var $parallaxBg = $('header.parallax-bg');
+
+    $documentElement.on('scroll', function() {
+        var currPosition = $documentElement.scrollTop();
+        $parallaxBg.css('background-position', '0 ' + -currPosition/4 + 'px');
+    });
+
+
+    /* -----------------------------------------------------------------------------------
+     * Animated Progress Bars
+     * -----------------------------------------------------------------------------------*/
+    $('.skills li').each(function () {
+        $(this).appear(function() {
+            $(this).animate({opacity:1,left:"0px"},1200);
+            var b = $(this).find("span").attr("data-width");
+            $(this).find("span").animate({
+                width: b + "%"
+            }, 1700, "easeOutCirc");
+        });
+    });
+
+    /* -----------------------------------------------------------------------------------
+     * Go back to the contact div after sending message
+     * -----------------------------------------------------------------------------------*/
     if (window.location.href.indexOf('/contact') != -1) {
         console.log("been here");
         scrollToSection('footer', 0);
     }
 
+    /* -----------------------------------------------------------------------------------
+     * Make the projects collapsable one at a time
+     * -----------------------------------------------------------------------------------*/
     $('.thumb-collapse').on('click',function(){
         $('.collapse').collapse('hide');
     });
 
+
+    /* -----------------------------------------------------------------------------------
+     * Enable menu scrolling
+     * -----------------------------------------------------------------------------------*/
     $('body').scrollspy({ target: '.navbar', offset: 70 });
 
     $('.scroll-link').on('click', function(event){
@@ -24,12 +60,6 @@ function main() {
 
 
 }
-
-function moveToSection(id, speed) {
-    var section = '#' + id;
-    var offset = $(section).offset().top - 50;
-}
-
 
 function scrollToSection(id, speed) {
     var section = '#' + id;
