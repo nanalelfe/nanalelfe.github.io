49a4fb875c27721108df0cb65a1510a42581c4ab
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-09-02 19:28:45 -0400

    Added content for solutions.repo project

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function main() {
         var $genSkills = $('div.resume-item.skills .gen-skills');
         $genSkills.html('');
 
-        $genSkills.first().append('<h4>General</h4> <p>Java, Python, C, Javascript, Android, Bash</p>');
+        $genSkills.first().append('<h4>General</h4> <p>Java, Python, C, Javascript, Android, Bash, Verilog, Assembly</p>');
         $genSkills.first().append('<h4>Databases</h4> <p>MongoDB</p>');
         $genSkills.first().append('<h4>Web</h4> <p>HTML5/CSS3, jQuery, Node.js, Handlebars.js, Bootstrap</p>');
         $genSkills.first().append('<h4>Tools</h4> <p>Vim, Eclipse, Webstorm, Wing</p>');
